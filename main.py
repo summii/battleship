ca0d29a82b6ad6207ba44b8eef957b9b2def4752
summii <sumeetbhadana99@gmail.com>
2020-04-28 16:52:56 +0530

    add battleship render

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 
+
+class Battleship(object):
+    
+    @staticmethod
+    def build(head, length, direction):
+        body = []
+        for i in range(length):
+            if direction == "N":
+                el = (head[0], head[1] - i)
+            elif direction == "S":
+                el = (head[0], head[1] +i)
+            elif direction == "W":
+                el = (head[0]-i, head[1])
+            elif direction == "E":
+                el = (head[0] + i, head[1])
+            body.append(el)
+
+        return Battleship(body)
+
+
+    def __init__(self, body):
+        self.body = body
+
+
+b = Battleship.build((1,2), 5, "S")
+b2 = Battleship([(1,2),(2,2),(3,2), (4,2), (5,2)])
+
+
 def render(board_width, board_height, shots):
     header = "+" + "-" * board_width + "+"
     print(header)
@@ -17,8 +45,39 @@ def render(board_width, board_height, shots):
 
     print(header)
 
+def render_battleships(board_width, board_height, battleships):
+    header = "+" + "-" * board_width + "+"
+    print(header)
 
+    board = []
+
+    for _ in range(board_width):
+        board.append([None for _ in range(board_height)])
+
+    for b in battleships:
+        for x,y in b.body:
+            board[x][y] = "O"
+
+    for y in range(board_height):
+        row = []
+        for x in range(board_height):
+            row.append(board[x][y] or " ")
+        print("|" + "".join(row) + "|")
+    print(header)
+
+    
 if __name__ == "__main__":
+    battleships = [
+            Battleship.build((1,1),2, "N"),
+            Battleship.build((5,8),5, "N"),
+            Battleship.build((2,3), 4, "E")
+            ]
+    for b in battleships:
+        print(b.body)
+
+    render_battleships(10,10, battleships)
+    exit(0)
+
     shots = []
     while True:
         inp = input("Where do you want to shoot?\n")
