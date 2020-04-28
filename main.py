4658aec364e0480d6de086ad630a1500e3d6431e
summii <sumeetbhadana99@gmail.com>
2020-04-28 15:34:41 +0530

    user input and render

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 
+def render(board_width, board_height, shots):
+    header = "+" + "-" * board_width + "+"
+    print(header)
+    
+    shots_set = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots:
+                ch = "X"
+            else:
+                ch = " "
+            row.append(ch)
 
+        print("|" + "".join(row) + "|")
+
+    print(header)
 
 
 if __name__ == "__main__":
-    print("Hello World!")
+    shots = []
+    while True:
+        inp = input("Where do you want to shoot?\n")
+        xstr, ystr = inp.split(",")
+        x = int(xstr)
+        y = int(ystr)
+
+        shots.append((x,y))
+        render(10,10,shots)
