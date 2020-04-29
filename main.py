e9d3fe8ee7558458c38b319387a34ec3112f9e9f
summii <sumeetbhadana99@gmail.com>
2020-04-29 21:35:36 +0530

    added two-player support

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
+import copy
 
+
+class GameBoard(object):
+    def __init__(self, battleships, width, height):
+        self.battleships = battleships
+        self.shots = []
+        self.width = width
+        self.height = height
+
+    def take_shot(self, shot_location):
+        is_hit = False
+        for b in self.battleships:
+            idx = b.body_index(shot_location)
+            if idx is not None:
+                is_hit = True
+                b.hits[idx] = True
+                break
+
+        self.shots.append(Shot(shot_location, is_hit))
+
+    def is_game_over(self):
+        return all([b.is_destroyed() for b in self.battleships])
+
+        
+
+
+class Shot(object):
+    def __init__(self, location, is_hit):
+        self.location = location
+        self.is_hit = is_hit
 
 class Battleship(object):
     
@@ -21,46 +51,49 @@ class Battleship(object):
 
     def __init__(self, body):
         self.body = body
+        self.hits = [False] * len(body)
+
+    def body_index(self, location):
+        try:
+            return self.body.index(location)
+        except ValueError:
+            return None
+
+    def is_destroyed(self):
+        return all(self.hits)
 
 
 b = Battleship.build((1,2), 5, "S")
 b2 = Battleship([(1,2),(2,2),(3,2), (4,2), (5,2)])
 
 
-def render(board_width, board_height, shots):
-    header = "+" + "-" * board_width + "+"
-    print(header)
-    
-    shots_set = set(shots)
-    for y in range(board_height):
-        row = []
-        for x in range(board_width):
-            if (x,y) in shots:
-                ch = "X"
-            else:
-                ch = " "
-            row.append(ch)
 
-        print("|" + "".join(row) + "|")
-
-    print(header)
-
-def render_battleships(board_width, board_height, battleships):
-    header = "+" + "-" * board_width + "+"
+def render(game_board, show_battleships=False):
+    header = "+" + "-" *game_board.width + "+"
     print(header)
 
     board = []
 
-    for _ in range(board_width):
-        board.append([None for _ in range(board_height)])
+    for _ in range(game_board.width):
+        board.append([None for _ in range(game_board.height)])
 
-    for b in battleships:
-        for x,y in b.body:
-            board[x][y] = "O"
+    if show_battleships:
 
-    for y in range(board_height):
+        for b in game_board.battleships:
+            for x,y in b.body:
+                board[x][y] = "O"
+
+    for sh in game_board.shots:
+        x,y = sh.location
+        if sh.is_hit:
+            ch = "X"
+        else:
+            ch ="*"
+        board[x][y] = ch
+
+    for y in range(game_board.height):
         row = []
-        for x in range(board_height):
+        for x in range(game_board.height):
             row.append(board[x][y] or " ")
         print("|" + "".join(row) + "|")
     print(header)
@@ -69,21 +102,38 @@ def render_battleships(board_width, board_height, battleships):
 if __name__ == "__main__":
     battleships = [
             Battleship.build((1,1),2, "N"),
-            Battleship.build((5,8),5, "N"),
-            Battleship.build((2,3), 4, "E")
+         #   Battleship.build((5,8),5, "N"),
+         #   Battleship.build((2,3), 4, "E")
             ]
-    for b in battleships:
-        print(b.body)
 
-    render_battleships(10,10, battleships)
-    exit(0)
+    game_boards = [
+            GameBoard(battleships, 10, 10),
+            GameBoard(copy.deepcopy(battleships), 10, 10)
+    ]
 
-    shots = []
+    player_names = [
+            "summii1",
+            "summii2"
+    ]
+
+    offensive_idx = 0
+
     while True:
+        defensive_idx = (offensive_idx + 1) %2
+        defensive_board = game_boards[defensive_idx]
+        
+
+        print("%s YOUR TURN" % player_names[offensive_idx])
         inp = input("Where do you want to shoot?\n")
         xstr, ystr = inp.split(",")
         x = int(xstr)
         y = int(ystr)
 
-        shots.append((x,y))
-        render(10,10,shots)
+        defensive_board.take_shot((x,y))
+        render(defensive_board)
+
+        if defensive_board.is_game_over():
+            print("%s WIN!" % player_names[offensive_idx])
+            break
+
+        offensive_idx = defensive_idx
